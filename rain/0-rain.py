d8e7e59237bd6e47d9139b19344386a7cd17c1c9
thedavidemmanuel <d.emmanuel@alustudent.com>
2023-06-16 20:42:12 +0000

    Interview

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,56 +1,34 @@
 #!/usr/bin/python3
+"""
+    Python script that calculates how much water is retained after it rains
+"""
+
 def rain(walls):
-    """Calculates the amount of water retained after it rains.
-
-    Args:
-        walls: A list of non-negative integers representing the heights of walls with unit width 1.
-
-    Returns:
-        Integer indicating total amount of rainwater retained.
     """
+        Function that calculates how much water will be retained after it rains
+        Return:
+            int: amount of rainwater collected
+    """
+    if not walls:
+        return 0
 
-    # Initialize the amount of water retained
-    water = 0
+    water_collected = 0
+    left, right = 0, len(walls) - 1
+    max_left, max_right = walls[left], walls[right]
 
-    # Iterate through the walls
-    for i in range(1, len(walls) - 1):
-        # Find the highest wall to the left and right of the current wall
-        left_wall = max(walls[:i])
-        right_wall = max(walls[i + 1:])
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] > max_left:
+                max_left = walls[left]
+            else:
+                water_collected += max_left - walls[left]
+            left += 1
+        else:
+            if walls[right] > max_right:
+                max_right = walls[right]
+            else:
+                water_collected += max_right - walls[right]
+            right -= 1
 
-        # Calculate the amount of water that can be retained by the current wall
-        water += min(left_wall, right_wall) - walls[i]
-
-    return water
-
-
-def main():
-    # Test cases
-    walls = []
-    assert rain(walls) == 0
-
-    walls = [2, 0, 2]
-    assert rain(walls) == 4
-
-    walls = [0, 1, 0, 2, 0, 3, 0, 4]
-    assert rain(walls) == 6
-
-    walls = [1, 1, 2, 0, 1, 1, 1]
-    assert rain(walls) == 3
-
-    walls = [0, 2, 1, 0, 1, 3, 1, 2, 1, 1, 2, 1]
-    assert rain(walls) == 10
-
-    walls = [2, 0, 0, 0, 0, 3, 0]
-    assert rain(walls) == 6
-
-    walls = [1]
-    assert rain(walls) == 0
-
-    walls = [3, 3]
-    assert rain(walls) == 6
-
-
-if __name__ == "__main__":
-    main()
+    return water_collected
 
